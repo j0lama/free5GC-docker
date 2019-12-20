54b690e12a944e1f66bf6c7447ab9945f7f9b56f
j0lama <jon_larrea@hotmail.com>
2019-12-20 15:41:16 +0000

    eNB configuration solved. Still having the http problem

--- virtual-UE-eNB/run_enb.sh
+++ virtual-UE-eNB/run_enb.sh
@@ -5,4 +5,4 @@ if [ $# -ne 2 ]
     exit 1
 fi
 sleep 7
-srsenb /config/enb.conf.fauxrf --enb.name=dummyENB01 --enb.mcc=208 --enb.mnc=93 --enb.enb_id=18AF1 --enb.tac=0x0001 --enb.cell_id=01 --enb.mme_addr=192.168.26.20 --enb.gtp_bind_addr=192.168.26.60 --enb.s1c_bind_addr=192.168.26.60 --enb_files.rr_config=/config/rr.conf --enb_files.sib_config=/config/sib.conf --enb_files.drb_config=/config/drb.conf
+srsenb /config/enb.conf.fauxrf --enb.name=dummyENB01 --enb.mcc=208 --enb.mnc=93 --enb.enb_id=18AF1 --enb.tac=0x0001 --enb.cell_id=01 --enb.mme_addr=$1 --enb.gtp_bind_addr=$2 --enb.s1c_bind_addr=$2 --enb_files.rr_config=/config/rr.conf --enb_files.sib_config=/config/sib.conf --enb_files.drb_config=/config/drb.conf
